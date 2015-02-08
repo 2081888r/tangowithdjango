f59bd2add72acfce8fc03b67e83566e049187834
2081888r <chaos_daemon_1@hotmail.co.uk>
2015-02-08 17:15:23 +0000

    Chapter 8 - completed

--- rango/urls.py
+++ rango/urls.py
@@ -1,11 +1,19 @@
 from django.conf.urls import patterns, url
 from rango import views
 from rango import about
-
+#urlpatterns = patterns('',
+    #url(r'^$', views.index, name='index'),
+    #url(r'^about/$', views.about, name='about'),
+    #url(r'^add_category/$', views.add_category, name='add_category'), # NEW MAPPING!
+    #url(r'^category/(?P<category_name_url>\w+)$', views.category, name='category'),)
 urlpatterns = patterns('',
-                       url(r'^$',views.index, name='index'),
-                       url(r'^about/', about.index, name ='index'),
-                       url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
+    url(r'^$',views.index, name='index'),
+    url(r'^about/$', about.index, name ='index'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
+    url (r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name= 'add_page')
+
+
 
                        )
 
